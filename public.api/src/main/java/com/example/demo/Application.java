2fba0839a14a5a0dff9208f5b54ae6c389a9cd66
Vaibhav <gubbinar@usc.edu>
2024-11-26 22:57:35 -0800

    Initial commit

---
+++ public.api/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+}
---
+++ retriever.svc/src/test/java/com/team/retriever/svc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.retriever.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
